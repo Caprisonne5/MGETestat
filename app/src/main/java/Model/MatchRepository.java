195a0fcf7967438abc69869481c434ceb47f17f7
Nicolas Richard <nicolas.richard@ost.ch>
2023-11-22 22:12:27 +0100

    Code säubern

--- app/src/main/java/Model/MatchRepository.java
+++ app/src/main/java/Model/MatchRepository.java
@@ -22,26 +22,5 @@ public class MatchRepository {
         database.matchDao().insertMatch(match);
     }
     public static void addSet (MatchSet set) { database.matchDao().insertMatchSet(set); }
-    public static Match addMatch () {
-        Match match = new Match(1);
-
-        MatchSet matchSet1 = new MatchSet(match.MatchId);
-        MatchSet matchSet2 = new MatchSet(match.MatchId);
-
-
-        matchSet1.setPointsPlayer1(6);
-        matchSet1.setPointsPlayer2(7);
-
-        matchSet2.setPointsPlayer1(6);
-        matchSet2.setPointsPlayer2(4);
-
-        match.P1 = "Markus";
-        match.P2 = "Robert";
-
-        addSet(matchSet1);
-        addSet(matchSet2);
-        addMatch(match);
-        return match;
-    }
 }
 
